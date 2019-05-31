c314a461a13426bfd05a7c686ad0e2ba6897a604
Ricardo Tosin <tosinricardo@gmail.com>
2019-05-30 23:04:41 -0300

    change Sort to fit better in component

---
+++ readable/src/components/OrderBy.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import './App.css';
+import { sortPost } from '../actions/postActions';
+import {Button, Row, Col, Grid} from 'react-bootstrap'
+
+class OrderBy extends Component {
+  
+    render() {
+      return(
+        <Grid>
+                <Row>
+                    <Col lg={3} md={3} sm={12}>Order By:</Col> 
+                    <Col lg={3} md={3} sm={12}> 
+                        <Button bsStyle="primary" onClick={() => this.props.sortPost("voteScore")}>
+                            Score
+                        </Button>
+                    </Col>
+                    <Col lg={3} md={3} sm={12}> 
+                        <Button bsStyle="primary" onClick={() => this.props.sortPost("timestamp")}>
+                            Date
+                        </Button>
+                    </Col>
+                </Row>
+            </Grid>
+      )
+    }
+    
+  }
+  
+  const mapDispatchToProps = (dispatch) => {
+    return {
+        sortPost: (sortType) => dispatch(sortPost(sortType))
+    }
+  }
+
+export default connect(null, mapDispatchToProps)(OrderBy)
+
+
+
+
