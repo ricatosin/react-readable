f4a264683760eb5a1d5b61b9c433a19ba3b72754
Ricardo Tosin <tosinricardo@gmail.com>
2019-05-30 22:35:30 -0300

    refactoring PostDetail to solve Requirements

--- readable/src/components/PostDetail.js
+++ readable/src/components/PostDetail.js
@@ -15,65 +15,24 @@ class PostDetail extends Component {
     this.props.fetchComments(this.props.postid);
   }
   render() {
-    return (
-      <div className="main-page">
-        <Header />
-        <div className="col-xs-2"></div>
-        <div className="col-xs-6">
-          <div className="posts">
-            <h3>Posts</h3>
-              {this.props.allPosts
-                .filter(post => post.id === this.props.postid)
-                .map((post) => {
-                  if (post.deleted) {
-                    return <h3 key={post.id}>Post has been deleted</h3>
-                  } else {
-                    return (
-                      <div key={post.id} >
-                        <Post post={post} detail={true}/>
-                        <SortBar comment={true}></SortBar>
-                        <ol>
-                          {this.props.comments[this.props.postid] === undefined ? null : this.props.comments[this.props.postid]
-                            .sort(sortBy(this.props.sort.sortOrder + this.props.sort.sortCategory))
-                            .map((comment) => {
-                              if (!comment.deleted)
-                                return <Comment key={comment.id} comment={comment}/>
-                              return null
-                          })}
-                        </ol>
-                        <div className="add-post" >
-                          <Link to={{pathname: "/add/comment/" + post.id }}><Glyphicon glyph="plus"/></Link>
-                        </div>
-                      </div>
-                    )
-                  }
-                })
-              }
-          </div>  
-        </div>
-      </div>
-    );
-  }
+ 
 }
 
-function mapStateToProps ({posts, categories, comments}) {
+const mapStateToProps = ({ posts, comments }, ownProps) => {
   return {
-    allPosts: posts.posts,
-    allCategories: categories.categories,
-    comments: comments,
-    sort: posts,
+    post: posts.find(p => p.id === ownProps.match.params.id),
+    comments: comments
   }
 }
 
-function mapDispatchToProps (dispatch) {
+const mapDispatchToProps = (dispatch) => {
   return {
-    fetchComments: (data) => dispatch(fetchComments(data)),
+    getPost: (id) => dispatch(fetchPost(id)),
+    deleteCurrentPost: (id) => dispatch(deleteCurrentPost(id)),
+    getComment: (id) => dispatch(fetchAllCommentsForPost(id))
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PostDetail)
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
 
 
