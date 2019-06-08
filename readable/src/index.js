75c7e90bd2495b1e5b9e372029db484405d1c6d4
Ricardo Tosin <tosinricardo@gmail.com>
2019-06-07 22:55:12 -0300

    detail and category

--- readable/src/index.js
+++ readable/src/index.js
@@ -1,33 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from "react-router-dom";
-import './index.css';
-import App from './components/App';
-import * as registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware, compose } from 'redux'
-import reducer from './reducers'
-import {Provider} from 'react-redux'
-import thunk from 'redux-thunk';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(
-    reducer,
-    composeEnhancers(
-        applyMiddleware(thunk)
-    )
-)
-
-console.log(store.getState())
+import App from './routes/App';
+import store from './redux';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </BrowserRouter>, 
-    document.getElementById('root'));
-
-    registerServiceWorker.unregister();
-
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
